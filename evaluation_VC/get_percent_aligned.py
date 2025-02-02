649f8492b28da9d0179e1e267aa97be69e38539e
Itolstoganov <itolstoganov@gmail.com>
2025-02-02 22:50:37 +0100

    Various fixes, parameter tuning for dardel

--- evaluation_VC/get_percent_aligned.py
+++ evaluation_VC/get_percent_aligned.py
@@ -13,7 +13,7 @@ def count_mapped(sam_file):
     num_mapped = 0
     for read in SAM_file.fetch(until_eof=True):
         if read.is_mapped:
-            result += 1
+            num_mapped += 1
     return num_mapped     
 
 
@@ -29,14 +29,13 @@ def main(args):
     #     is_paf = True
     # alignments_tool = read_sam(args.tool)
 
-
     # if args.predicted_sam:
     #     predicted = read_sam(args.predicted_sam)
     # elif args.predicted_paf:
     #     predicted, mapped_to_multiple_pos = read_paf(args.predicted_paf)
     #     # print("Number of reads mapped to several positions (using first pos):", mapped_to_multiple_pos)
 
-    num_mapped = count_mapped(sam_file)
+    num_mapped = count_mapped(args.sam)
     print(round(num_mapped/8000000,5))
 
     # for ovl, nr_in_common in overlaps.items():
@@ -49,6 +48,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--tool', type=str, default="", help='The tool name')
     parser.add_argument('--outfile', type=str, default=None, help='Path to file.')
+    parser.add_argument('--sam', type=str, default=None, help='Path to file')
     # parser.set_defaults(which='main')
     args = parser.parse_args()
 
